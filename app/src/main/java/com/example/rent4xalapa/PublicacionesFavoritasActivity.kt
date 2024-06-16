7e44d81c9f2879995071a6b06a192431395b7290
Roberto <zS21016329@estudiantes.uv.mx>
2024-06-16 13:26:23 -0600

    Layout y creacion de activity CU-Publicaciones Favoritas

---
+++ app/src/main/java/com/example/rent4xalapa/PublicacionesFavoritasActivity.kt
@@ -0,0 +1,10 @@
+package com.example.rent4xalapa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PublicacionesFavoritasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
