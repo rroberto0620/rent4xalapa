b4193b37df2205715c7e62dfafdf62918c5a8e60
OmarVX <omarlomo22@gmail.com>
2024-06-17 01:26:18 -0600

    Lógica RevisaPublicaciones (talvez)

--- app/src/main/java/com/example/rent4xalapa/RevisarPublicacionesActivity.kt
+++ app/src/main/java/com/example/rent4xalapa/RevisarPublicacionesActivity.kt
@@ -2,18 +2,82 @@ package com.example.rent4xalapa
 
 import android.os.Bundle
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AlertDialog
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.rent4xalapa.databinding.RevisarPerfilBinding
 import com.example.rent4xalapa.databinding.RevisarPublicacionesBinding
+import com.example.rent4xalapa.modelo.PublicacionesBD
+import com.example.rent4xalapa.poko.Publicacion
+
 
 class RevisarPublicacionesActivity : AppCompatActivity() {
 
     private lateinit var binding: RevisarPublicacionesBinding
 
+    private var idPublicacion = 0
+    private var titulo = ""
+    private var descripcion = ""
+    private var direccion = ""
+    private var tipo = ""
+    private var numHabitaciones = ""
+    private var costo = 0
+    private var petFriendly = 0
+    private var servicios = 0
+    private var amueblado = 0
+    private var entradaCompartida = 0
+    private var cochera = 0
+    private var aire = 0
+    private var calificacion = 0
+    private var idUsuario = 0
+
+    private lateinit var modelo : PublicacionesBD
+    private lateinit var array: ArrayList<Publicacion>
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = RevisarPublicacionesBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        modelo=PublicacionesBD(this@RevisarPublicacionesActivity)
+
+        array = arrayListOf<Publicacion>()
+        array = modelo.obtenerPublicaciones(idUsuario)
+
+        idPublicacion = intent.getIntExtra("idPublicacion", 0)
+        titulo = intent.getStringExtra("titulo")!!
+        descripcion = intent.getStringExtra("descripcion")!!
+        direccion = intent.getStringExtra("direccion")!!
+        tipo = intent.getStringExtra("tipo")!!
+        numHabitaciones = intent.getStringExtra("numero de habitaciones")!!
+        costo = intent.getIntExtra("costo", 0)
+        petFriendly = intent.getIntExtra("petFriendly", 0)
+        servicios = intent.getIntExtra("servicios", 0)
+        amueblado = intent.getIntExtra("amueblado", 0)
+        entradaCompartida = intent.getIntExtra("entrada compartida", 0)
+        cochera = intent.getIntExtra("cochera", 0)
+        aire = intent.getIntExtra("aire acondicionado", 0)
+        calificacion = intent.getIntExtra("calificacion", 0)
+        idUsuario = intent.getIntExtra("idUsuario", 0)
+
+
+        binding.tvNombreDepa.setText("Titulo: "+"$titulo")
+        binding.tvNombreDepa.setText("Descripcion: "+"$descripcion")
+        binding.tvNombreDepa.setText("Direccion: "+"$direccion")
+        binding.tvNombreDepa.setText("Tipo: "+"$tipo")
+        binding.tvNombreDepa.setText("Numero de habitaciones: "+"$numHabitaciones")
+        binding.tvNombreDepa.setText("Costo: "+"$costo")
+        binding.tvdetallesLugar.setText("Pet Friendly: "+"$petFriendly")
+        binding.tvdetallesLugar.setText("Servicios: "+"$petFriendly")
+        binding.tvdetallesLugar.setText("Amueblado: "+"$petFriendly")
+        binding.tvdetallesLugar.setText("Entrada Compartida: "+"$petFriendly")
+        binding.tvdetallesLugar.setText("Tiene cochera: "+"$petFriendly")
+        binding.tvdetallesLugar.setText("Aire acondicionado: "+"$petFriendly")
+        binding.tvdetallesLugar.setText("Calificacion: "+"$calificacion")
+
 
 
         binding.btnCalificar.setOnClickListener {
@@ -21,7 +85,7 @@ class RevisarPublicacionesActivity : AppCompatActivity() {
         }
 
         binding.btnRealizar.setOnClickListener {
-//            irPantallaRealizarCita()
+            irPantallaRealizarCita()
         }
     }
 
@@ -52,9 +116,9 @@ class RevisarPublicacionesActivity : AppCompatActivity() {
     }
 
 
-//    fun irPantallaRealizarCita(){
-//        val intent = Intent(this@RevisarPublicaciones, RealizarCita::class.java)
-//        startActivity(intent)
-//        finish()
-//    }
+    fun irPantallaRealizarCita(){
+        val intent = Intent(this@RevisarPublicacionesActivity, RealizarCitaActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
