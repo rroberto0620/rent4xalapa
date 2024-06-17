4ee0aa1af0c625dcd01c4206ad720d533be9e02c
OmarVX <omarlomo22@gmail.com>
2024-06-16 18:55:52 -0600

    Actividad RealizarPublicaciones agregada

---
+++ app/src/main/java/com/example/rent4xalapa/RealizarPublicaciones.kt
@@ -0,0 +1,13 @@
+package com.example.rent4xalapa
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RealizarPublicaciones : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
